1c191e62c3c66eb11fc6d743b914ad37a6103a60
Ramel Cabug-os <tearhear18@gmail.com>
2025-06-21 11:07:27 +0800

    update

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,4 +1,7 @@
 class EventsController < ApplicationController
+  
+  skip_before_action :verify_authenticity_token, only: [:create]
+  
   def create
     @event = Event.new(event_params)
     if @event.save
