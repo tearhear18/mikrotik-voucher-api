4e47949cadaa00f6430d642d089a680f1fba602d
Ramel Cabug-os <tearhear18@gmail.com>
2025-06-22 20:37:42 +0800

    update

--- app/controllers/stations_controller.rb
+++ app/controllers/stations_controller.rb
@@ -3,15 +3,16 @@ class StationsController < ApplicationController
     @stations = Station.all    
     time_zone = "Asia/Manila"
     today = Time.zone.now.to_date.strftime('%Y-%m-%d')
-    @event_counts = Event
+    
+    @login_counts = Event
       .where(mode: "login", created_at: 24.hours.ago..Time.current)
-      .group_by_hour(
-        :created_at,
-        format: "%Y-%m-%d %H:%M",
-        series: false,
-        time_zone: time_zone
-      )
-      .count    
+      .group_by_hour(:created_at, format: "%Y-%m-%d %H:%M", series: false, time_zone: time_zone)
+      .count
+
+    @logout_counts = Event
+      .where(mode: "logout", created_at: 24.hours.ago..Time.current)
+      .group_by_hour(:created_at, format: "%Y-%m-%d %H:%M", series: false, time_zone: time_zone)
+      .count
     raw_data = Voucher.where('created_at >= ?',10.days.ago).group(:station_id).group_by_day(:created_at, time_zone: time_zone).sum(:amount)
     @per_station_daily_sales = raw_data.each_with_object({}) do |((station_id, date), price), result|
       agent_name = Station.find(station_id).name
