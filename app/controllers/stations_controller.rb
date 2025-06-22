88c4e2d16d9465e26c5e4d817ce1d0f413252dd9
Ramel Cabug-os <tearhear18@gmail.com>
2025-06-22 20:08:11 +0800

    update

--- app/controllers/stations_controller.rb
+++ app/controllers/stations_controller.rb
@@ -1,6 +1,9 @@
 class StationsController < ApplicationController
   def index
-    @stations = Station.all
+    @stations = Station.all    
+    @event_counts = Event
+      .where(mode: "login",created_at: 24.hours.ago..Time.current)
+      .group_by_hour(:created_at, format: "%Y-%m-%d %H:%M", series: false).count
   end
 
   def show
