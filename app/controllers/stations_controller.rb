686fbda041ca9a41d4aee6084193f4275e81bcf7
Ramel Cabug-os <tearhear18@gmail.com>
2025-06-25 06:34:36 +0800

    Fix

--- app/controllers/stations_controller.rb
+++ app/controllers/stations_controller.rb
@@ -2,7 +2,7 @@ class StationsController < ApplicationController
   def index
     @stations = Station.all
     time_zone = "Asia/Manila"
-    today = Date.current.in_time_zone(time_zone).strftime("%Y-%m-%d")
+    today = Time.current.in_time_zone(time_zone).strftime("%Y-%m-%d")
 
     start_of_day = Time.now.in_time_zone(time_zone).beginning_of_day
     end_of_day   = Time.now.in_time_zone(time_zone).end_of_day
@@ -22,10 +22,10 @@ class StationsController < ApplicationController
       
     raw_data = Voucher.where("created_at >= ?", 10.days.ago).group(:station_id).group_by_day(:created_at, time_zone: time_zone).sum(:amount)
     
+    
     @per_station_daily_sales = raw_data.each_with_object({}) do |((station_id, date), price), result|
       agent_name = Station.find(station_id).name
       result[agent_name] ||= {}
-
       if today.to_s === date.to_s
         result[agent_name]["Today"] = price
       else
