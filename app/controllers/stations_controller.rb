f53621556a16d5158d9fccfdcdf1c952b84a6516
Ramel Cabug-os <tearhear18@gmail.com>
2025-06-24 21:49:05 +0800

    update

--- app/controllers/stations_controller.rb
+++ app/controllers/stations_controller.rb
@@ -19,12 +19,13 @@ class StationsController < ApplicationController
       .where(mode: "logout", created_at: 24.hours.ago..Time.current)
       .group_by_hour(:created_at, format: "%Y-%m-%d %H:%M", series: false, time_zone: time_zone)
       .count
+      
     raw_data = Voucher.where("created_at >= ?", 10.days.ago).group(:station_id).group_by_day(:created_at, time_zone: time_zone).sum(:amount)
+    
     @per_station_daily_sales = raw_data.each_with_object({}) do |((station_id, date), price), result|
       agent_name = Station.find(station_id).name
       result[agent_name] ||= {}
 
-      result[agent_name][date] = price
       if today.to_s === date.to_s
         result[agent_name]["Today"] = price
       else
